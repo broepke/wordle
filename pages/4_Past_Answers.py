7dbb4532358bcbfaaec744ba7bb854ccd92d7602
Brian Roepke <broepke@gmail.com>
2024-12-20 10:00:04 -0600

    sorting

--- pages/4_Past_Answers.py
+++ pages/4_Past_Answers.py
@@ -20,5 +20,17 @@ combined_df['Date'] = pd.to_datetime(combined_df['Date'], format='mixed', errors
 # Drop the index from the Dataframe
 combined_df.reset_index(drop=True, inplace=True)
 
+# Sort the DataFrame by Date
+combined_df.sort_values(by='Date', ascending=False, inplace=True)
+
 # Display the Dataframe
 st.dataframe(combined_df, use_container_width=True)
+
+# Add a download button for the DataFrame
+csv = combined_df.to_csv(index=False)
+st.download_button(
+    label="Download data as CSV",
+    data=csv,
+    file_name='past_wordle_answers.csv',
+    mime='text/csv',
+)
